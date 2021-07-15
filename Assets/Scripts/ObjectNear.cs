fd507287d15af68adfb4e22844520de26ad17ca2
Jean-Philippe Bourdais <jeanphi.bourdais@gmail.com>
2021-07-15 14:48:57 +0200

    add animation in script

--- Assets/Scripts/ObjectNear.cs
+++ Assets/Scripts/ObjectNear.cs
@@ -9,7 +9,13 @@ public class ObjectNear : MonoBehaviour
     private GameObject card1;
 
     [SerializeField]
+    private GameObject model1;
+
+    [SerializeField]
     private GameObject card2;
+
+    [SerializeField]
+    private GameObject model2;
 
     [SerializeField]
     private int distanceFromCard;
@@ -20,11 +26,14 @@ public class ObjectNear : MonoBehaviour
     private bool stopAndPlay;
     private bool inputChanged;
 
-    private Animation anim;
+    private Animation animCard1Model;
+    private Animation animCard2Model;
 
     // Start is called before the first frame update
     void Start()
     {
+        animCard1Model = model1.GetComponent<Animation>();
+        animCard2Model = model2.GetComponent<Animation>();
         stopAndPlay = false;
     }
 
@@ -33,20 +42,21 @@ public class ObjectNear : MonoBehaviour
     {
         if (Vector3.Distance(card1.transform.position, card2.transform.position) < distanceFromCard)
         {
-            Debug.Log(stopAndPlay);
             if (!stopAndPlay)
             {
-                //Debug.Log("OK");
                 stopAndPlay = true;
                 music.Play();
+                animCard1Model.Play("animation");
+                animCard2Model.Play("animation");
             }
         } else
         {
-            Debug.Log(stopAndPlay);
             if (stopAndPlay)
             {
                 stopAndPlay = false;
                 music.Stop();
+                animCard1Model.Stop("animation");
+                animCard2Model.Stop("animation");
             }
         }
     }
