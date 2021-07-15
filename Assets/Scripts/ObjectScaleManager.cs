b7e86dbed583e510ff43c456d240b1fbe2add734
Jean-Philippe Bourdais <jeanphi.bourdais@gmail.com>
2021-07-15 10:02:27 +0200

    add characters with textures

---
+++ Assets/Scripts/ObjectScaleManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectScaleManager : MonoBehaviour
+{
+
+    public float scale = 0.01f;
+    private Vector2 startPosition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleScaleChange();
+    }
+
+    private void HandleScaleChange()
+    {
+        Touch[] touches = Input.touches;
+        if (touches.Length >= 1)
+        {
+            Touch currentTouch = touches[0];
+            if (currentTouch.phase == TouchPhase.Began)
+            {
+                startPosition = currentTouch.position;
+            }
+            else if (currentTouch.phase == TouchPhase.Ended)
+            {
+                Vector2 endPosition = currentTouch.position;
+                HandleSwipe(startPosition, endPosition);
+            }
+        }
+    }
+
+    private void HandleSwipe(Vector2 startPosition, Vector2 endPosition)
+    {
+        bool isUpwardSwipe = startPosition.y < endPosition.y;
+        bool isDownwardSwipe = startPosition.y > endPosition.y;
+
+        //float swipeDistance = Vector2.Distance(startPosition, endPosition);
+        if (isUpwardSwipe)
+        {
+            transform.localScale += new Vector3(scale, scale, scale);
+        }
+        else if (isDownwardSwipe)
+        {
+            transform.localScale -= new Vector3(scale, scale, scale);
+        }
+    }
+}
